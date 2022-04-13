fa5da14164a14f7ba4e4656e6633eec920de208c
roman <romanfree1311@gmail.com>
2022-04-13 20:56:06 +0300

    homework 5

--- src/main/java/com/geekbrains/MyThread.java
+++ src/main/java/com/geekbrains/MyThread.java
@@ -1,4 +1,27 @@
 package com.geekbrains;
 
-public class MyThread {
+public class MyThread extends Thread{
+    private float[] arr;
+
+    MyThread(String name, float[] arr){
+        super(name);
+        this.arr = arr;
+    }
+
+    float[] getArr(){
+        return arr;
+    }
+
+    @Override
+    public void run(){
+        calculate();
+    }
+
+    private void calculate(){
+        int len = arr.length;
+
+        for(int i = 0; i < len; i++){
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
 }
